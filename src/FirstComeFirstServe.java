9f476d8168fb7c9f07a7da5ce7cffb44e33e550e
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-08-21 22:26:12 +1000

    Updated naming

--- src/FirstComeFirstServe.java
+++ src/FirstComeFirstServe.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class FirstComeFirstServe extends Algorithm{
-    
+    public FirstComeFirstServe(int _dispatcherTime, ProcessQueue<Process> _processQueue){
+        super(_dispatcherTime, _processQueue);
+    }
+
+    public void runAlgo(){
+        
+    }
+
+    public String getAlgoStats(){
+        return "";
+    }
 }
--- src/NarrowRoundRobin.java
+++ src/NarrowRoundRobin.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class NarrowRoundRobin extends Algorithm{
-    
+    public NarrowRoundRobin(int _dispatcherTime, ProcessQueue<Process> _processQueue){
+        super(_dispatcherTime, _processQueue);
+    }
+
+    public void runAlgo(){
+
+    }
+
+    public String getAlgoStats(){
+        return "";
+    }
 }
