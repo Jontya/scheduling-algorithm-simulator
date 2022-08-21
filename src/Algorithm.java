70fcf997492cea026cb214e3032bc6f9cfb462f4
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-08-21 22:26:43 +1000

    Added ready queue

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+
 public abstract class Algorithm {
-    
+    private static int dispatcherTime;
+
+    protected int currTime;
+    protected int timeQuantum;
+    protected ProcessQueue<Process> processQueue;
+
+    public Algorithm(int _dispatcherTime, ProcessQueue<Process> _processQueue){
+        currTime = 0;
+
+        dispatcherTime = _dispatcherTime;
+        processQueue = _processQueue;
+    }
+
+    public int getDispTime(){
+        return dispatcherTime;
+    }
+
+    public abstract void runAlgo();
+
+    public abstract String getAlgoStats();
 }
