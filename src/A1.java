bfc49e29193622e9789773773cd0310c74cc3356
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-08-23 09:38:21 +1000

    Updated for testing

--- src/A1.java
+++ src/A1.java
@@ -65,7 +65,8 @@ public class A1 {
     private void run(){
         FirstComeFirstServe FCFS = new FirstComeFirstServe(dispatcherTime, processQueue);
         RoundRobin RR = new RoundRobin(dispatcherTime, processQueue, 4);
-        FCFS.runAlgo();
+        NarrowRoundRobin NRR = new NarrowRoundRobin(dispatcherTime, processQueue, 4);
+        NRR.runAlgo();
     }
 
     private void printResults(){
