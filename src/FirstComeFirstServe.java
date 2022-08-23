acf42e027da36bad4a43c4f4133fabcdc62e1ae2
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-08-23 23:01:28 +1000

    Updated the way stats are stored

--- src/FirstComeFirstServe.java
+++ src/FirstComeFirstServe.java
@@ -1,6 +1,8 @@
 public class FirstComeFirstServe extends Algorithm{
     public FirstComeFirstServe(int _dispatcherTime, ProcessQueue<Process> _processQueue){
         super(_dispatcherTime, _processQueue);
+        timeQuantum = 0;
+        algoName = "FCFS";
     }
 
     public void runAlgo(){
@@ -10,18 +12,19 @@ public class FirstComeFirstServe extends Algorithm{
         }
 
         while(readyQueue.getSize() != 0){
-            currProcess = readyQueue.pop();
             currTime += getDispTime();
+            currProcess = readyQueue.pop();
+
+            dispatcherEvents.add("T" + currTime + ": " + currProcess.getId() + "\n");
+
             currTime += currProcess.getExecSize();
-            currProcess.setProcessingStats(currTime - currProcess.getArriveTime(), currTime - currProcess.getServiceTime() - currProcess.getArriveTime(), currTime);
-            System.out.println(currProcess.processInfo());
+
+            String[] processingEvent = {currProcess.getId(), Integer.toString(currTime - currProcess.getArriveTime()), Integer.toString(currTime - currProcess.getExecSize() - currProcess.getArriveTime())};
+            processingEvents.add(processingEvent);
+
             if(processQueue.getSize() != 0){
                 primeReadyQueue();
             }
         }
-    }
-
-    public String getAlgoStats(){
-        return "";
     }
 }
