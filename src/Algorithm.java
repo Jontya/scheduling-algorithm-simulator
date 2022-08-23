a178d207eb669a1d4552cca5e8fb2c52ae84a29a
Jonty Atkinson <jontyatkinson2002@gmail.com>
2022-08-23 23:02:04 +1000

    Algorithm stat output and formatting

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,12 +1,23 @@
+import java.util.ArrayList;
+import java.io.Console;
+
 public abstract class Algorithm {
     private static int dispatcherTime;
 
+    protected String algoName;
     protected Process currProcess;
     protected int currTime;
     protected int timeQuantum;
 
     protected ProcessQueue<Process> processQueue;
     protected ProcessQueue<Process> readyQueue;
+
+    protected ArrayList<String> dispatcherEvents;
+    protected ArrayList<String[]> processingEvents;
+
+    String fmt = "%1$4s %2$10s %3$10s%n";  
+    static Console col = System.console();  
+    
 
     public Algorithm(int _dispatcherTime, ProcessQueue<Process> _processQueue){
         currTime = 0;
@@ -16,6 +27,8 @@ public abstract class Algorithm {
         currProcess = null;
 
         readyQueue = new ProcessQueue<>();
+        dispatcherEvents = new ArrayList<>();
+        processingEvents = new ArrayList<>();
     }
 
     public int getDispTime(){
@@ -36,7 +49,21 @@ public abstract class Algorithm {
         }
     }
 
-    public abstract void runAlgo();
+    public String getAlgorithmStats(){
+        String algorithmStats = algoName + ":\n";
+        for(int i = 0; i < dispatcherEvents.size(); i++){
+            algorithmStats += dispatcherEvents.get(i);
+        }
 
-    public abstract String getAlgoStats();
+        // TO DO: Sort processing events queue, output to file and make algos only run once when they have one item left
+
+        algorithmStats += "\nProcess Turnaround Time Waiting Time\n";
+        for(int i = 0; i < processingEvents.size(); i++){
+            algorithmStats += String.format("%-7s %-15s %-1s", processingEvents.get(i)[0], processingEvents.get(i)[1], processingEvents.get(i)[2]) + "\n";
+    
+        }
+        return algorithmStats;
+    }
+
+    public abstract void runAlgo();
 }
